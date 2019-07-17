04f0ec8dce8ee67e43e65d1807a7c24a23f3a830
sunakan <sunakan03@gmail.com>
2019-07-17 20:16:49 +0900

    [Add] 一般ユーザのuidとgidを変更する

--- digdag-ls-docker/entrypoint.sh
+++ digdag-ls-docker/entrypoint.sh
@@ -2,6 +2,5 @@
 
 usermod -u ${UID:-25252} $EXECUTE_USER
 groupmod -g ${GID:-25252} $EXECUTE_USER
-#chown -R sunakan-dev $INDEX_DATA_PATH
 
-exec /usr/sbin/gosu sunakan-dev "$@"
+exec /usr/sbin/gosu $EXECUTE_USER "$@"
