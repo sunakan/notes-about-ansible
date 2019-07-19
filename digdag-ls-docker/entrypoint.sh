a0bc4563cd477892f40d0f3fc5c982f9ed30161e
sunakan <sunakan03@gmail.com>
2019-07-19 22:03:00 +0900

    [Add] digdagのサンプルを追加

--- digdag-ls-docker/entrypoint.sh
+++ digdag-ls-docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-usermod -u ${UID:-25252} $EXECUTE_USER
-groupmod -g ${GID:-25252} $EXECUTE_USER
+#usermod -u ${UID:-25252} $EXECUTE_USER
+#groupmod -g ${GID:-25252} $EXECUTE_USER
 
-exec /usr/sbin/gosu $EXECUTE_USER "$@"
+#exec /usr/sbin/gosu $EXECUTE_USER "$@"
