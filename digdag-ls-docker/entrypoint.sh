a50d2d47b3516de073a528d66854f3a68a9150e2
sunakan <sunakan03@gmail.com>
2019-07-20 16:56:54 +0900

    [Add] とりあえずdigdagを動かす

--- digdag-ls-docker/entrypoint.sh
+++ digdag-ls-docker/entrypoint.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-#usermod -u ${UID:-25252} $EXECUTE_USER
-#groupmod -g ${GID:-25252} $EXECUTE_USER
+CURRENT_USER=$(whoami)
+EXEC_USER=${EXEC_USER:-suna-dev}
 
-#exec /usr/sbin/gosu $EXECUTE_USER "$@"
+if [ $CURRENT_USER = root ] && [ $EXEC_USER != root ]; then
+  groupadd -g ${EXEC_UID:-25252} $EXEC_USER
+  useradd  -g $EXEC_USER -u ${EXEC_UID:-25252} $EXEC_USER
+  chown -hR $EXEC_USER $WORKING_HOME
+  if [ $IS_MASTER = true ]; then
+    chown -hR $EXEC_USER $DATA_HOME
+  fi
+fi
+
+exec /usr/sbin/gosu $EXEC_USER "$@"
